681165804a466a592ef85c58def854afe984da4f
unknown <tommyshelbypbgp@gmail.com>
2022-07-10 21:05:05 +0530

    Configure React app for deployment to github pages

---
+++ src/container/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import SearchBox from "../SearchBox.js"
+import CardList from "../component/CardList.js";
+import "./App.css"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ""
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({robots: users }))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        }
+        else {
+            return (
+                <div className="tc" >
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div >
+            );
+
+        }
+    }
+}
+export default App
