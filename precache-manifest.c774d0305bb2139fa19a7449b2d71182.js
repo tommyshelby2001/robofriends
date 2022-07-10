df9192eea467e280f092d60eefe3cbf33ff55a45
unknown <tommyshelbypbgp@gmail.com>
2022-07-10 20:07:24 +0530

    Updates

---
+++ precache-manifest.c774d0305bb2139fa19a7449b2d71182.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3592b31044b5cddcf13",
+    "url": "/robofriends/static/css/main.8ace1aa6.chunk.css"
+  },
+  {
+    "revision": "d3592b31044b5cddcf13",
+    "url": "/robofriends/static/js/main.d3592b31.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "1429c54c2a1733b32bb8",
+    "url": "/robofriends/static/css/2.604540b7.chunk.css"
+  },
+  {
+    "revision": "1429c54c2a1733b32bb8",
+    "url": "/robofriends/static/js/2.1429c54c.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "4039be0397a782d0170e7ef1d587797b",
+    "url": "/robofriends/index.html"
+  }
+];
